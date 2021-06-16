dd458b97c8ec0a4f695f23f4fb13c30c656b5dcd
cbermudez13 <36652402+cbermudez13@users.noreply.github.com>
2021-06-15 22:30:18 -0400

    Update header.js

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,26 +1,5 @@
-import Button from '@material-ui/core/Button'
-import PropTypes from 'prop-types'
-/*export default function Header(){
+export default function Header(){
     return <div>2021 IBM Accelerate Software Track Lab Boiler Plate</div>
 }
-*/
 
-export const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click')
-    }
-    return (
-        <header>
-            <h1>{title}</h1>
-            <Button variant="contained" color="primary" onClick = {onClick}> 
-                Add Task </Button>
-        </header>
-    )
-}
-Header.defaultProps = {
-    title: 'IBM Task Tracker',
-}
-Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
-export default Header
+
